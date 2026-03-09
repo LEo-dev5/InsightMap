65b1bb14d203a1ef963dd431f57522d89be4eb57
LEo-dev5 <kang203062@gmail.com>
2026-03-09 22:48:34 +0900

    article test

--- crawler.py
+++ crawler.py
@@ -1,9 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-
+from newspaper import Article
 url = "https://n.news.naver.com/article/001/0015077942"
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-print(soup.get_text())
+article = Article(url, language='ko')
+article.download()
+article.parse()
+print(article.text)
