fc3bd49a72a5f7d3170c059de530646bcb960721
LEo-dev5 <kang203062@gmail.com>
2026-04-05 13:02:30 +0900

    노드 디자인 변경 및 우측 뉴스 패널 추가

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ def get_nodes():
     for article in articles:
         result = summarizer.summarize_article(article)
         nodes.append(result)
-    return nodes
+
+    edges = []
+    for i in range(len(nodes)):
+        for j in range(i + 1, len(nodes)):
+            keywords_i = set(nodes[i]["keywords"])
+            keywords_j = set(nodes[j]["keywords"])
+            if keywords_i & keywords_j:
+                edges.append({"from": i, "to": j})
+
+    return {"nodes": nodes, "edges": edges}
 
 from fastapi.responses import FileResponse
 
